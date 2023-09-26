5b10132be07e12d05cdc39087327a20ffb04c0ae
Vignesh Narayanaswamy <56098083+vignesh-nswamy@users.noreply.github.com>
2023-09-26 09:37:31 -0400

    Update sample-usage.sh
    Signed-off-by: Vignesh Narayanaswamy <56098083+vignesh-nswamy@users.noreply.github.com>

--- sample-usage.sh
+++ sample-usage.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+if [ -f ./.env ]; then
+  export $(grep -v '^#' ./.env | xargs)
+  echo "Variables exported from .env."
+else
+  echo "Error: .env file not found."
+  exit 1
+fi
+
 openai-processor process --model gpt-4 \
   --infile ./examples/input.jsonl \
   --outfile ./examples/output.jsonl \
